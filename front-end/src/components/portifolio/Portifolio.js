68349578b321ca0c946772220d5d10b0f81cc529
gizaw <gizawag123@gmail.com>
2024-11-16 16:30:02 +0300

    project updates

--- front-end/src/components/portifolio/Portifolio.js
+++ front-end/src/components/portifolio/Portifolio.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "./portifolio.css";
 import { urlFor, client } from "../../client";
+import projects from "../../data/projectsData";
 
 const Portfolio = () => {
-  const [projects,setProjects] = useState([])
-   
-  useEffect(() => {
-     const query = `*[_type == "projects"]`;
-     client.fetch(query).then((data) => {
-       setProjects(data);
-     });
-   }, []);
+  // const myprojects = projects;
+
+  // useEffect(() => {
+  //   const query = `*[_type == "projects"]`;
+  //   client.fetch(query).then((data) => {
+  //     setProjects(data);
+  //   });
+  // }, []);
+
   return (
     <section id="portfolio">
       <h2>Projects</h2>
@@ -20,18 +22,31 @@ const Portfolio = () => {
         {projects.map((project, index) => (
           <article className="portfolio__item" key={index}>
             <div className="portfolio__item-image">
-              <img src={urlFor(project.imgurl)} alt="" />
+              <img src={project.imgurl} alt={project.title} />
             </div>
             <h3>{project.title}</h3>
 
+            <div className="portfolio__item-techstack">
+              {project.techStack.map((tech, idx) => (
+                <span key={idx} className="tech-chip">
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            {/* Add project description with scrolling */}
+            <div className="portfolio__item-description">
+              {project.description}
+            </div>
+
             <div className="portfolio__item-cta">
               <div>
-                <a href={`${project.githubLink}`} className="btn">
+                <a href={`${project.githubLink}`}   target="_blank" className="btn">
                   Github
                 </a>
               </div>
               <div>
-                <a href={`${project.demoLink}`} className="btn btn-primary">
+                <a href={`${project.demoLink}`}   target="_blank" className="btn btn-primary">
                   Live Demo
                 </a>
               </div>
---
+++ front-end/src/data/projectsData.js
@@ -0,0 +1,126 @@
+const projects = [
+  {
+    title: "Blockchain Document Verification",
+    description:
+      "A blockchain-based application designed to help educational institutions and employers verify various types of documents securely using blockchain technology.",
+    imgurl: "/assets/project.doc_verification.png",
+    githubLink: "https://github.com/eagles-projects/document-validation",
+    demoLink: "https://document-validation.onrender.com/",
+    techStack: ["Next.js", "Node.js", "Blockchain"]
+  },
+
+  {
+    title: "Angular Admin Dashboard",
+    description:
+      "An Angular-based dashboard featuring graphs, charts, and management tools for users and other datasets.",
+    imgurl: "/assets/project.angular_dashboard.png",
+    githubLink: "https://github.com/Gizaw-Agodo/angular-dashboard-project",
+    demoLink: "https://angular-dashboard-gz.netlify.app/",
+    techStack: ["Angular", "Node.js", "PostgreSQL"]
+  },
+ 
+  {
+    title: "Heart Disease Prediction",
+    description:
+      "An AI-powered project that assists medical institutions in predicting heart diseases using machine learning algorithms.",
+    imgurl: "/assets/project.heartdisiease_prediction.png",
+    githubLink: "https://github.com/Gizaw-Agodo/Heart-Deasease-Pridictor-ML",
+    demoLink: "https://github.com/Gizaw-Agodo/Heart-Deasease-Pridictor-ML",
+    techStack: ["Python", "Machine Learning"]
+  },
+
+  {
+    title: "Crypto Transact",
+    description:
+      "An application that facilitates cryptocurrency transactions efficiently and securely.",
+    imgurl: "/assets/crypto_project.png",
+    githubLink: "https://github.com/Gizaw-Agodo/crypto-transact",
+    demoLink: "https://crypto-transact.netlify.app/",
+    techStack: ["React", "Node.js", "Blockchain"]
+  },
+
+  {
+    title: "Cocoon Restaurant Website",
+    description:
+      "A web application that enables restaurants to accept orders, list products, and manage deliveries effortlessly.",
+    imgurl: "/assets/project.cocoon_website.png",
+    githubLink: "https://github.com/Gizaw-Agodo/cocoon-restaurant-website",
+    demoLink: "https://github.com/Gizaw-Agodo/cocoon-restaurant-website",
+    techStack: ["React", "Node.js", "MongoDB"]
+  },
+  {
+    title: "Online Clearance System",
+    description:
+      "A system that allows educational institutions to issue clearances online, streamlining the process for students and universities and reducing manual work.",
+    imgurl: "/assets/project.clerance_system.png",
+    githubLink: "https://github.com/Gizaw-Agodo/online-clearance-system",
+    demoLink: "https://github.com/Gizaw-Agodo/online-clearance-system",
+    techStack: ["Spring Boot", "Java", "MySQL"]
+  },
+  {
+    title: "COVID-19 Tracker",
+    description:
+      "An application that visually displays global COVID-19 statistics using graphs and numerical data updated daily.",
+    imgurl: "/assets/project.covid_tracker.png",
+    githubLink: "https://my-portifolio-2.netlify.app/none",
+    demoLink: "https://worlds-covid-traker.netlify.app/",
+    techStack: ["React.js", "Bootstrap"]
+  },
+  {
+    title: "Habesha E-commerce",
+    description:
+      "An e-commerce platform showcasing Ethiopian traditional clothes, facilitating online orders and sales.",
+    imgurl: "/assets/project.ecommerce.png",
+    githubLink: "https://github.com/eagles-projects/document-validation",
+    demoLink: "https://document-validation.onrender.com/",
+    techStack: ["React.js", "Node.js", "PostgreSQL"]
+  },
+  {
+    title: "React Admin Dashboard",
+    description:
+      "A React-based dashboard featuring graphs, charts, and management tools for products, customers, transactions, and subscriptions.",
+    imgurl: "/assets/project.react_dashboard.png",
+    githubLink: "https://github.com/Gizaw-Agodo/Fullstack-Admin-Dashboard",
+    demoLink: "https://admin-frontends-2vnq.onrender.com/",
+    techStack: ["React.js", "Node.js", "MongoDB"]
+  },
+  
+  {
+    title: "Travel Advisor",
+    description:
+      "A travel companion application utilizing Google Maps API to display nearby hotels and tourist attractions conveniently.",
+    imgurl: "/assets/project.travel_advisor.png",
+    githubLink: "https://my-portifolio-2.netlify.app/none",
+    demoLink: "https://around-you.netlify.app/",
+    techStack: ["React.js", "Node.js", "GraphQL"]
+  },
+  {
+    title: "YouTube Clone",
+    description:
+      "A project that replicates the core functionality and interface of YouTube.",
+    imgurl: "/assets/project.youtubeclone.png",
+    githubLink: "https://github.com/Gizaw-Agodo/you-tube-clone",
+    demoLink: "https://youtube-clone-gizaw.netlify.app/",
+    techStack: ["React.js", "Node.js", "MySQL"]
+  },
+  {
+    title: "Crime Reporter App",
+    description:
+      "A mobile application that allows users to report crimes to the police and enables police to respond promptly.",
+    imgurl: "/assets/crime_reporter_project.png",
+    githubLink: "https://github.com/project-two",
+    demoLink: "https://demo-project-two.com",
+    techStack: ["Flutter", "Node.js", "MongoDB"]
+  },
+  {
+    title: "To-Do App",
+    description:
+      "A simple to-do management application for organizing daily tasks effectively.",
+    imgurl: "/assets/todo_project.png",
+    githubLink: "https://github.com/Gizaw-Agodo/todolist",
+    demoLink: "https://gizaw-agodo.github.io/practic/",
+    techStack: ["HTML", "CSS", "Node.js"]
+  }
+];
+
+export default projects;
